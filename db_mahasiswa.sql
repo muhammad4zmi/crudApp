45c76a0263badd8c80d3d5d0275ea7ca4d79288a
Muhammad Azmi <muhammad4zmi@gmail.com>
2024-08-08 18:50:34 +0700

    Add files via upload

---
+++ db_mahasiswa.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 08, 2024 at 11:48 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mahasiswa`
+--
+
+CREATE TABLE `tbl_mahasiswa` (
+  `nim` int NOT NULL,
+  `nama_mahasiswa` varchar(25) DEFAULT NULL,
+  `jenis_kelamin` enum('P','L') DEFAULT NULL,
+  `alamat` varchar(50) DEFAULT NULL,
+  `prodi` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_mahasiswa`
+--
+
+INSERT INTO `tbl_mahasiswa` (`nim`, `nama_mahasiswa`, `jenis_kelamin`, `alamat`, `prodi`) VALUES
+(11111, 'Azmi', 'P', 'Lombok', 'Sistem Informasu'),
+(123123, 'azmi aja', 'L', 'qweqwe', 'weqwe'),
+(31423414, 'hsadahsjd', 'L', 'hashjas', 'Sistem Infoirmasi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_mahasiswa`
+--
+ALTER TABLE `tbl_mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
